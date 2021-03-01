d546d07edfc8136995a3ca9915b447263eac2dc4
thealibrs <alibrs15@hotmail.com>
2021-03-01 16:48:52 +0200

    final updates

---
+++ constants.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+WELCOME_MESSAGE = """Evveet Sigmoid burada!🙂\nSanırım ne yapacağını bilemedin, sorun yok işte komutlarım:\n
+- /sondakika son dakika haberleri getiririm
+- /kurlar dolar, euro, vs. merak edersen onlar burda
+- /kitapbul dilediğin bir kitap varsa pdf'i burada bulabilirim belki
+- /gunlukburc istediğin burcun günlük yorumu burda
+- /havadurumu merak ettiğin şehrin 4 günlük hava raporu
+                 """
+
+
+TOKEN = "YOUR-TELEGRAM-BOT-TOKEN"
+
+
+SORRY_MESSAGE = "Ahh bazen kafam gidiyor...😔\n Şu an yalnızca şu komutları anlayabiliyorum...\n\n - /sondakika \n - /kitapbul kitap adi\n - /kurlar \n -/gunlukburc burcun \n -/havadurumu sehir"
+
+START_MESSAGE = """Evveet Sigmoid burada!🙂\nİşte komutlarım\n
+- /sondakika son dakika haberleri getiririm
+- /kurlar dolar, euro, vs. merak edersen onlar burda
+- /kitapbul istediğin bi' kitap varsa yazman yeterli
+- /gunlukburc istediğin burcun günlük yorumu burda
+- /havadurumu merak ettiğin şehrin 4 günlük hava raporu
+                 """
